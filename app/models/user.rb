9c6f1f42fc29b617fba91947c78ecaf76523a07a
sandiegodj <davidjanczyn@gmail.com>
2014-12-20 00:27:58 +0000

    complete basic friend association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
   has_one :profile, dependent: :destroy, autosave: true
   accepts_nested_attributes_for :profile
   has_many :posts
+  has_many :friends, :through => :friendship, :conditions => { "friendships.status" => "accepted"}
+  has_many :pending_friends, :through => :friendship, :source => :friend, :conditions => {"friendships.status" => "pending"}, :order => :created_at
+  has_many :friendship, dependent: :destroy
 end
