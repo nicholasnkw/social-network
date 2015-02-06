07ef80ecba23bdbf3b81330fc30e23e7268146fc
sandiegodj <davidjanczyn@gmail.com>
2015-02-06 20:56:36 +0000

    style user index page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,6 +6,6 @@ class Profile < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
 
   # Paperclip
-  has_attached_file :photo, :styles => { :medium => "400x400>", :thumb => "50x50>" }
+  has_attached_file :photo, :styles => { :medium => "400x400>", :thumb => "50x50>", :regular => "200x200>" }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
