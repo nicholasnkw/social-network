8672efe37f437a1c67d3e75a03c03df78ea06da6
sandiegodj <davidjanczyn@gmail.com>
2015-02-06 23:29:21 +0000

    remove user mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
-  
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: "Welcome to The Social Network!")
-  end
-end
